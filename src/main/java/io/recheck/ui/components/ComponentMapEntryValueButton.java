ab29fb85fed98950045614c1ff4b274b870b2e00
Ivaylo Rusev <irusev@recheck.io>
2021-04-13 21:03:52 +0300

    delete ComponentMapEntryValueButton for now

--- src/main/java/io/recheck/ui/components/ComponentMapEntryValueButton.java
+++
@@ -1,9 +0,0 @@
-package io.recheck.ui.components;
-
-import com.vaadin.flow.component.button.Button;
-
-public interface ComponentMapEntryValueButton {
-
-    Button newButton(String value);
-    String getValue();
-}
